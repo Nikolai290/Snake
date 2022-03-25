7bd83ec626c1271f598dd91afe16633552e2d6e5
Solomatov Nikolai <nikosol94@gmail.com>
2022-03-25 16:55:00 +0700

    snake move troubles

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -28,6 +28,7 @@ namespace Snake {
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,6 +41,7 @@ namespace Snake {
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
             // 
             // pictureBox1
             // 
@@ -55,18 +57,31 @@ namespace Snake {
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(581, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,6 +90,7 @@ namespace Snake {
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -17,35 +17,34 @@ namespace Snake {
             timer1.Interval = INIT_TIMER_INERVAL;
         }
 
+        public int Counter { get; set; }
         private IList<Cell> snake { get; set; } = new List<Cell>();
+        private Cell food { get; set; }
 
         private Direction direction { get; set; } = Direction.Up;
 
         private Random rnd = new Random();
 
         private void button1_Click(object sender, EventArgs e) {
-            if(snake.Count > 0) {
-                foreach(var cell in snake) {
+            Counter = 0;
+            label1.Text = Counter.ToString();
+            if (food == null) {
+                food = new Cell(SpawnLabel(Color.Red), rnd.Next(WIDTH_CELLS), rnd.Next(HEIGHT_CELLS));
+            } else {
+                food.Go(rnd.Next(WIDTH_CELLS), rnd.Next(HEIGHT_CELLS));
+            }
+
+            if (snake.Count > 0) {
+                foreach (var cell in snake) {
                     cell.label.Dispose();
                 }
                 snake.Clear();
             }
             var head = SpawnLabel(Color.Blue);
 
-            snake = new List<Cell> { new Cell(head, 5, 5) };
+            snake = new List<Cell> { new Cell(head, 0, 5) };
             direction = Direction.Rigth;
             timer1.Start();
-        }
-
-        private Label SpawnLabel(Color color) {
-            return new Label() {
-                AutoSize = false,
-                Width = CELL_SIZE,
-                Height = CELL_SIZE,
-                BackColor = color,
-                Text = "",
-                Parent = pictureBox1,
-            };
         }
 
 
@@ -54,22 +53,11 @@ namespace Snake {
             MoveSnake();
         }
 
-        private void MoveSnake() {
-            for(var i = snake.Count-1; i >= 0; i--) {
-                if(i == 0) {
-                    snake[i].Go(direction);
-                } else {
-                    snake[i].Go(snake[i-1].X, snake[i-1].Y);
-                }
-            }
-            if (CheckWallCollision() || CheckSnakeCollision()) {
-                timer1.Stop();
-            }
-        }
-
 
 
         private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            label1.Text = e.KeyCode.ToString();
+            ChangeDirection(e.KeyCode);
             switch (e.KeyCode) {
                 case Keys.Up:
                     direction = Direction.Up;
@@ -88,17 +76,95 @@ namespace Snake {
             }
         }
 
+        private bool CheckFoodCollision() {
+            return snake.First().Collision(food.X, food.Y);
+        }
+
         private bool CheckWallCollision() {
             return snake.Any(x => x.Die);
         }
 
         private bool CheckSnakeCollision() {
             foreach (var cell in snake) {
-                if(snake.Any(x => x != cell && x.Collision(cell.X, cell.Y))) {
+                if (snake.Any(x => x != cell && cell.Collision(x.X, x.Y))) {
                     return true;
                 }
             }
             return false;
+        }
+
+        private void ChangeDirection(Keys key) {
+            switch (key) {
+                case Keys.W:
+                    direction = Direction.Up;
+                    return;
+                case Keys.D:
+                    direction = Direction.Rigth;
+                    return;
+                case Keys.S:
+                    direction = Direction.Down;
+                    return;
+                case Keys.A:
+                    direction = Direction.Left;
+                    return;
+                default:
+                    return;
+            }
+        }
+
+        private Label SpawnLabel(Color color) {
+            return new Label() {
+                AutoSize = false,
+                Width = CELL_SIZE,
+                Height = CELL_SIZE,
+                BackColor = color,
+                Text = "",
+                Parent = pictureBox1,
+            };
+        }
+
+
+        private void MoveSnake() {
+            int xMem = snake.First().X;
+            int yMem = snake.First().Y;
+            snake.First().Go(direction);
+            if (CheckFoodCollision() && snake.Count > 1) {
+                xMem = snake.Last().X;
+                yMem = snake.Last().Y;
+            }
+
+            for (var i = snake.Count - 1; i > 0; i--) {
+                snake[i].Go(snake[i - 1].X, snake[i - 1].Y);
+            }
+            if (CheckFoodCollision()) {
+                snake.Add(new Cell(SpawnLabel(Color.LightBlue), xMem, yMem));
+                Counter++;
+                label1.Text = Counter.ToString();
+                while (true) {
+                    var newX = rnd.Next(WIDTH_CELLS);
+                    var newY = rnd.Next(HEIGHT_CELLS);
+                    if(snake.All(x => !x.Collision(newX, newY))) {
+                        food.Go(newX, newY);
+                        break;
+                    }
+                }
+
+            }
+            if (CheckWallCollision() || CheckSnakeCollision()) {
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e) {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e) {
+
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e) {
+            ChangeDirection(e.KeyCode);
         }
     }
 }
